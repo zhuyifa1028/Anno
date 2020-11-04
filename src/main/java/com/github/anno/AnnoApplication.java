591a465dd26cd62ae77922e6686734ca8dfe9c48
夜语丶 <zhuyifa1028@gmail.com>
2020-11-04 18:06:56 +0800

    安诺2.0初始化

---
+++ src/main/java/com/github/anno/AnnoApplication.java
@@ -0,0 +1,19 @@
+package com.github.anno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 安诺
+ *
+ * @author zhuyifa
+ * @version 2020-11-02
+ */
+@SpringBootApplication
+public class AnnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/anno/AnnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.anno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
